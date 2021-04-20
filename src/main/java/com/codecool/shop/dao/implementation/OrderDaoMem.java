12674bfd2c9378f7c16dabc631f49fe6782f1fd3
zsomborr <zsomborkincses@gmail.com>
2021-04-20 15:37:07 +0200

    feat: orderdao interface and service implemented

---
+++ src/main/java/com/codecool/shop/dao/implementation/OrderDaoMem.java
@@ -0,0 +1,50 @@
+package com.codecool.shop.dao.implementation;
+
+
+import com.codecool.shop.dao.OrderDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.model.Product;
+import com.codecool.shop.model.ProductCategory;
+import com.codecool.shop.model.Supplier;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OrderDaoMem implements OrderDao {
+
+    private List<Product> data = new ArrayList<>();
+    private static OrderDaoMem instance = null;
+
+    /* A private Constructor prevents any other class from instantiating.
+     */
+    private OrderDaoMem() {
+    }
+
+    public static OrderDaoMem getInstance() {
+        if (instance == null) {
+            instance = new OrderDaoMem();
+        }
+        return instance;
+    }
+
+    @Override
+    public void add(Product product) {
+        data.add(product);
+    }
+
+    @Override
+    public Product find(int id) {
+        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    }
+
+    @Override
+    public void remove(int id) {
+        data.remove(find(id));
+    }
+
+    @Override
+    public List<Product> getAll() {
+        return data;
+    }
+}
