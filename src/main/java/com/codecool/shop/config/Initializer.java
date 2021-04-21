b627335693b071aea2a213400ac00f806559a3a1
zsomborr <zsomborkincses@gmail.com>
2021-04-21 17:07:05 +0200

    Changes by havacs

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -36,9 +36,9 @@ public class Initializer implements ServletContextListener {
         productCategoryDataStore.add(shirt);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "USD", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "USD", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("shirt", 0, "USD", "test2", shirt, amazon));
+        productDataStore.add(new Product("Amazon Fire", 49.9f, "EUR", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
+        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "EUR", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
+        productDataStore.add(new Product("Amazon Fire HD 8", 89, "EUR", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
+        productDataStore.add(new Product("Terray shirt", 18, "EUR", "A marvelous shirt with our mentor's beautiful face on it. Good quality with handsomeness.", shirt, amazon));
     }
 }
