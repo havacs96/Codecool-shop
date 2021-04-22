3ec7c83814ff6e7fe247c896c4a832f7e589a11e
zsomborr <zsomborkincses@gmail.com>
2021-04-22 17:55:03 +0200

    Changes by havacs

--- src/main/java/com/codecool/shop/config/Initializer.java
+++ src/main/java/com/codecool/shop/config/Initializer.java
@@ -24,21 +24,25 @@ public class Initializer implements ServletContextListener {
         SupplierDao supplierDataStore = SupplierDaoMem.getInstance();
 
         //setting up a new supplier
-        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-        supplierDataStore.add(amazon);
-        Supplier lenovo = new Supplier("Lenovo", "Computers");
-        supplierDataStore.add(lenovo);
+        Supplier razer = new Supplier("Razor", "Softwares and hardwares for your computer");
+        supplierDataStore.add(razer);
+        Supplier original = new Supplier("Original", "Special CodeCool products");
+        supplierDataStore.add(original);
 
         //setting up a new product category
-        ProductCategory tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        productCategoryDataStore.add(tablet);
-        ProductCategory shirt = new ProductCategory("shirt", "test", "test");
+        ProductCategory hardware = new ProductCategory("Hardware", "Computer", "Hardware for your computer, that it deserves.");
+        productCategoryDataStore.add(hardware);
+        ProductCategory coolStuff = new ProductCategory("Cool stuff", "CodeCool", "Products that are going to say others: THIS GUY/GIRL KNOW PROGRAMMING!");
+        productCategoryDataStore.add(coolStuff);
+        ProductCategory shirt = new ProductCategory("shirt", "CodeCool", "Original CodeCool products");
         productCategoryDataStore.add(shirt);
 
         //setting up products and printing it
-        productDataStore.add(new Product("Amazon Fire", 49.9f, "EUR", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon));
-        productDataStore.add(new Product("Lenovo IdeaPad Miix 700", 479, "EUR", "Keyboard cover is included. Fanless Core m5 processor. Full-size USB ports. Adjustable kickstand.", tablet, lenovo));
-        productDataStore.add(new Product("Amazon Fire HD 8", 89, "EUR", "Amazon's latest Fire HD 8 tablet is a great value for media consumption.", tablet, amazon));
-        productDataStore.add(new Product("Terray shirt", 18, "EUR", "A marvelous shirt with our mentor's beautiful face on it. Good quality with handsomeness.", shirt, amazon));
+        productDataStore.add(new Product("Razor mouse", 28, "EUR", "Fantastic price. Beautiful white mouse with 3500 maximum DPI.", hardware, razer));
+        productDataStore.add(new Product("CodeCoolest sunglass", 8, "EUR", "Comfortable sunglasses for hot summers.", coolStuff, original));
+        productDataStore.add(new Product("CodeCoolers Backpack", 89, "EUR", "It is a perfect choice for you outdoor activities.", coolStuff, original));
+        productDataStore.add(new Product("Terray shirt", 22, "EUR", "A marvelous shirt with our mentor's beautiful face on it. Good quality with handsomeness.", shirt, original));
+        productDataStore.add(new Product("Casual CodeCool shirt", 18, "EUR", "A nice looking white shirt. Make your CodeCooler friends jealous wearing it!", shirt, original));
+        productDataStore.add(new Product("CodeCool headset", 30, "EUR", "A black headset with microphone. You can hear your friends cleaner then ever!", hardware, razer));
     }
 }
